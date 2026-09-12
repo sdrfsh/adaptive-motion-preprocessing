d8d38429240519bd57f871a650868be5323b358a
Sahar Darafsh <sahar.drfsh@gmail.com>
2026-09-12 15:35:38 +0200

    feat: median noise reducer added

--- src/amprep/__init__.py
+++ src/amprep/__init__.py
@@ -1,5 +1,11 @@
 from amprep.background_subtractor import BackgroundSubtractor
-from amprep.noise_reducer import NoiseReducer
+from amprep.noise_reducer import MedianNoiseReducer, NoiseReducer
 from amprep.types import Frame, MotionImage
 
-__all__ = ["BackgroundSubtractor", "Frame", "MotionImage", "NoiseReducer"]
+__all__ = [
+    "BackgroundSubtractor",
+    "Frame",
+    "MedianNoiseReducer",
+    "MotionImage",
+    "NoiseReducer",
+]
