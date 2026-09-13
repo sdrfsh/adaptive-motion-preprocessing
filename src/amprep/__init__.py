5cca0b88e7b02402df1c383983a1bd67f177ea4a
Sahar Darafsh <sahar.drfsh@gmail.com>
2026-09-13 17:15:13 +0200

    fix: no need to set window

--- src/amprep/__init__.py
+++ src/amprep/__init__.py
@@ -1,7 +1,7 @@
 from amprep.background_subtractor import BackgroundSubtractor, KNNBackgroundSubtractor
 from amprep.noise_reducer import MedianNoiseReducer, NoiseReducer
 from amprep.preprocessor import AdaptiveMotionPreprocessor
-from amprep.types import Frame, MotionImage, Window
+from amprep.types import Frame, MotionImage
 
 __all__ = [
     "AdaptiveMotionPreprocessor",
@@ -11,5 +11,4 @@ __all__ = [
     "MedianNoiseReducer",
     "MotionImage",
     "NoiseReducer",
-    "Window",
 ]
